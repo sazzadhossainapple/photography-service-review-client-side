c051d96d9870da61927fa9150f5e3554ee051757
sazzadhossainapple <sazzadapple81@gmail.com>
2022-11-09 11:48:44 +0600

    deleted added

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { AuthContext } from "../../context/UserContext";
 import MyReviewsDetails from "./MyReviewsDetails";
 
@@ -14,34 +15,59 @@ const MyReviews = () => {
         setMyReview(data.data);
       });
   }, [user?.email]);
+
+  //delete by review
+  const handleReviewDelete = (id) => {
+    // toast.success("are you sure you want to the delete");
+    // console.log("delete");
+    fetch(`http://localhost:5000/myReview/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status) {
+          toast.success("review deleted by successfully.");
+          const remaining = myReview.filter((reviews) => reviews._id !== id);
+          setMyReview(remaining);
+        }
+      });
+  };
+
   return (
     <div className="min-h-screen py-12 lg:px-20 md:px-12 sm:px-8 px-6">
-      {myReview.length === 0 && (
-        <h1 className="text-xl">No reviews were added</h1>
+      {myReview.length === 0 ? (
+        <>
+          <h1 className="text-xl flex justify-center items-center min-h-screen ">
+            No reviews were added
+          </h1>
+        </>
+      ) : (
+        <>
+          <div className="overflow-x-auto">
+            <table className="table w-full">
+              <thead>
+                <tr className="bg-[#ab1818]">
+                  <th></th>
+                  <th>Service Name</th>
+                  <th>Email</th>
+                  <th>Review</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {myReview.map((review, index) => (
+                  <MyReviewsDetails
+                    key={review._id}
+                    review={review}
+                    index={index + 1}
+                    handleReviewDelete={handleReviewDelete}
+                  ></MyReviewsDetails>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
-
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          <thead>
-            <tr className="bg-[#ab1818]">
-              <th></th>
-              <th>Service Name</th>
-              <th>Email</th>
-              <th>Review</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {myReview.map((review, index) => (
-              <MyReviewsDetails
-                key={review._id}
-                review={review}
-                index={index + 1}
-              ></MyReviewsDetails>
-            ))}
-          </tbody>
-        </table>
-      </div>
     </div>
   );
 };
--- src/pages/MyReviews/MyReviewsDetails.js
+++ src/pages/MyReviews/MyReviewsDetails.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const MyReviewsDetails = ({ review, index }) => {
-  const { serviceTitle, reviewMassage, email } = review;
+const MyReviewsDetails = ({ review, index, handleReviewDelete }) => {
+  const { _id, serviceTitle, reviewMassage, email } = review;
 
   return (
     <tr>
@@ -11,7 +11,10 @@ const MyReviewsDetails = ({ review, index }) => {
       <td>{email}</td>
       <td>{reviewMassage}</td>
       <td>
-        <button className="btn text-slate-200 bg-[#ab1818] hover:bg-[#9c1616] hover:border-[#9c1616] border-[#ab1818] px-7 font-medium">
+        <button
+          onClick={() => handleReviewDelete(_id)}
+          className="btn text-slate-200 bg-[#ab1818] hover:bg-[#9c1616] hover:border-[#9c1616] border-[#ab1818] px-7 font-medium mr-3"
+        >
           delete
         </button>
         <Link className="btn text-slate-200 bg-[#ab1818] hover:bg-[#9c1616] hover:border-[#9c1616] border-[#ab1818] px-7 font-medium">
