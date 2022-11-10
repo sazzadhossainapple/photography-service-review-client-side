2baf98ca5557f29e50405dafa0ff1493a0eadd04
sazzadhossainapple <sazzadapple81@gmail.com>
2022-11-10 19:04:09 +0600

    home updated

--- src/pages/Shared/NabBanner/NabBanner.js
+++ src/pages/Shared/NabBanner/NabBanner.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "../../../asserts/images/bannerImage.jpg";
 import "../Navbar/Navbar.css";
 import { AiOutlineDoubleRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const NabBanner = () => {
   return (
@@ -20,11 +21,13 @@ const NabBanner = () => {
             your different <br /> tasks and projects. Photographing your stories
             and memories for life.
           </p>
-          <button className="border px-8 text-base py-3 font-medium hover:border-[#ab1818] rounded-full hover:bg-[#ab1818] bg-transparent text-white">
-            <p className="flex items-center justify-center  gap-2 ">
-              <span> Get Started </span>| <AiOutlineDoubleRight />
-            </p>
-          </button>
+          <Link to="/blog">
+            <button className="border px-8 text-base py-3 font-medium hover:border-[#ab1818] rounded-full hover:bg-[#ab1818] bg-transparent text-white">
+              <p className="flex items-center justify-center  gap-2 ">
+                <span> Get Started </span>| <AiOutlineDoubleRight />
+              </p>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
